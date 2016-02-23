485db2929c9284305789f9449acdd31e6c6d36f1
gj <gj@itcast.cn>
2016-02-23 18:09:08 +0800

    初始化项目

---
+++ 自定义不等高的cell-frame方式/自定义不等高的cell-frame方式/Controller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  自定义不等高的cell-frame方式
+//
+//  Created by apple on 16/2/23.
+//  Copyright © 2016年 m14a.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UITableViewController
+
+
+@end
+
---
+++ 自定义不等高的cell-frame方式/自定义不等高的cell-frame方式/Controller/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  自定义不等高的cell-frame方式
+//
+//  Created by apple on 16/2/23.
+//  Copyright © 2016年 m14a.cn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end
